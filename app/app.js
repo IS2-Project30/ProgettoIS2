a3e39c7ad163809e1f6615d317f8f378c61f64dc
Diego Cappelletti <diego.cappelletti@studenti.unitn.it>
2020-11-18 17:29:53 +0100

    aggiunto .gitignore, agiunto il file principale index.js, aggiornato il file pakage.json e app.js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,8 @@
 var express = require('express');
 var app = express();
 
-var port = 3000;
+app.get('/', function(req, res){ 
+    res.send('Hello World!');
+});
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,5 @@
+const app = require('./app/app.js');
+
+var port = 3000;
+
+app.listen(port);
