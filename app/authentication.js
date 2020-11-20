9b168b355a7dc69f00a612738adfee317d5abff9
Marco Crespi <marco.crespi@studenti.unitn.it>
2020-11-20 16:14:16 +0100

    Modificati file authentication.js, index.html, script.js con aggiunta temporanea funzionalità controllo login

--- app/authentication.js
+++ app/authentication.js
@@ -1,9 +1,43 @@
 const express = require('express');
-const router = express.Router;
+var mongoose = require('mongoose');
+var User = require('./models/User');
+const router = express.Router();
+
+var cerca = async function(email, pass){
+    var res;
+    try{
+        res = await User.find({email: email, password: pass}).exec();
+    } catch(err){
+        console.log('Errore find');
+    }
+
+    console.log(res);
+
+    if(res.length){
+        //return 0;
+        console.log('Trovato');
+    } else {
+        //return 1;
+        console.log('Non trovato');
+    }
+
+}
 
 router.post('/', async function(req, res){
 
-    console.log(req.body.email);
+    mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true });
+
+    cerca(req.body.email, req.body.password);
+/*  
+    if(cerca(req.body.email, req.body.password)){
+        console.log('Trovato');
+    } else {
+        console.log('Non trovato');
+    }
+*/
+
+    console.log(req.body.email + " " + req.body.password);
+
 });
 
 router.get('/', function(req, res){ 
--- static/script.js
+++ static/script.js
@@ -2,15 +2,29 @@ const { getMaxListeners } = require("../app/app");
 
 function login(){
 
-    var email = document.getElementById("loginemail").value;
+    var email = document.getElementById("loginemail");
+    var pass = document.getElementById("password");
 
-    console.log(email);
+    if(!email.checkValidity()){
+        document.getElementById("errore").innerHTML = email.validationMessage;
+        return;
+    }
+    if(!pass.checkValidity()){
+        document.getElementById("errore").innerHTML = pass.validationMessage;
+        return;
+    }
+    document.getElementById("errore").innerHTML = "";
 
-    var a = {email:email};
+    //console.log(email.value + " " + pass.value);
+
+    var a = {email:email.value, password:pass.value};
+
+    //console.log(a.email + " " + a.password);
 
     fetch('../authentication',{
         method:'post',
         headers:{'content-type':'application/json'},
         body:JSON.stringify(a)
     });
+
 }
